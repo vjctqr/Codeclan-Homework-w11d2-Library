f57524b63c6e831ced852ec2f1f3db9e476f6013
Victor <victorsetlhare@gmail.com>
2020-06-16 16:17:11 +0100

    book construct

---
+++ src/main/java/Book.java
@@ -0,0 +1,14 @@
+public class Book {
+
+    private String title;
+    private String author;
+    private String genre;
+
+    public Book(String title, String author, String genre){
+        this.title = title;
+        this.title = author;
+        this.title = genre;
+    }
+
+
+}
---
+++ src/test/java/BookTest.java
@@ -0,0 +1,13 @@
+import org.junit.Before;
+
+public class BookTest {
+    private Book book;
+
+    @Before
+    public void before(){
+        book = new Book("Eat and Run", "Scott Jurek", "Running and Jogging")
+    }
+
+
+
+}
