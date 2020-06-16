e3fdd9ed3d17f25fe5dbf6950467d7cac47af1ff
Victor <victorsetlhare@gmail.com>
2020-06-16 16:49:39 +0100

    library constructor added

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,12 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private int stock;
+    private ArrayList<Book> books;
+
+    public Library(int stock){
+        this.stock = stock;
+        this.books = new ArrayList<Book>();
+    }
 }
