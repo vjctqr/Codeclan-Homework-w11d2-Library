a86088c7252724bed60748856a3e86d557bdf54f
Victor <victorsetlhare@gmail.com>
2020-06-16 16:28:00 +0100

    book 3 added

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -7,7 +7,8 @@ public class BookTest {
     public void before(){
         book = new Book("Eat and Run", "Scott Jurek", "Running and Jogging");
         book = new Book("Born to Run", "Christopher McDoughall", "Running and Jogging");
-
+        book = new Book("Twenty Chickens For A Saddle", "Robin Scott", "Social Biography");
+        book = new Book()
     }
 
 
